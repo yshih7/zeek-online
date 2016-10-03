25b40b0633e58e2f20b541887eb34433bafad178
Stephen Cohen <stesen@outlook.com>
2016-10-03 16:20:06 -0400

    Add Aurelia file structure
    Using the Aurelia Webpack ESNext skeleton

---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Penultima Online';
+    config.map([
+      { route: ['', 'home'], name: 'home',      moduleId: './home',      nav: true, title: 'Home' }
+    ]);
+
+    this.router = router;
+  }
+}
---
+++ client/src/main.js
@@ -0,0 +1,16 @@
+// we want font-awesome to load as soon as possible to show the fa-spinner
+import '../styles/styles.css';
+import 'font-awesome/css/font-awesome.css';
+
+// comment out if you don't want a Promise polyfill (remove also from webpack.common.js)
+import * as Bluebird from 'bluebird';
+Bluebird.config({ warnings: false });
+
+export async function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging();
+
+  await aurelia.start();
+  aurelia.setRoot('app');
+}
