e3d28b08079009705aee4f45731b82f8eb1f59c8
Stephen Cohen <stesen@outlook.com>
2016-10-13 18:21:55 -0400

    Implement signup front-end

--- client/src/views/signup.js
+++ client/src/views/signup.js
@@ -1,4 +1,65 @@
+import {inject} from "aurelia-framework";
+import {Router} from "aurelia-router";
+import {validate as validateEmail} from "email-validator";
+import {users} from "../services";
+
+const ERR_BAD_EMAIL = "That's not a valid email address. Try again.";
+const ERR_PASSWORD_MISMATCH = "Your passwords don't match. Try again.";
+const ERR_EMAIL_TAKEN = "That email's already in use. Try again.";
+const ERR_GENERIC = "Something went wrong. Please try again later.";
+
+@inject(Router)
 export class Signup
 {
-    
+    email = "";
+    password = "";
+    passwordConfirm = "";
+    displayName = "";
+
+    error = "";
+
+    router;
+
+    constructor(router)
+    {
+        this.router = router;
+    }
+
+    async send()
+    {
+        if (!validateEmail(this.email))
+        {
+            this.error = ERR_BAD_EMAIL;
+            return;
+        }
+
+        if (this.password !== this.passwordConfirm)
+        {
+            this.error = ERR_PASSWORD_MISMATCH;
+            return;
+        }
+
+        try
+        {
+            await users.create({
+                email: this.email,
+                password: this.password,
+                displayName: this.displayName
+            });
+        }
+        catch (err)
+        {
+            if (err.type === "Conflict" && err.data.reason === "ERR_EMAIL_TAKEN")
+            {
+                this.error = ERR_EMAIL_TAKEN;
+            }
+            else
+            {
+                this.error = ERR_GENERIC;
+            }
+            return;
+        }
+
+        this.router.navigateToRoute("login", {signupSuccess: true});
+    }
 }
