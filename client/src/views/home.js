464f0abeb9497f17dfb9c0512a6a44379352d869
Stephen Cohen <stesen@outlook.com>
2016-10-13 19:44:21 -0400

    Implement login

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,4 +1,17 @@
+import app from "../services";
+
 export class Home
 {
+    loggedIn = false;
+    displayName = "";
 
+    activate()
+    {
+        const user = app.get("user");
+        if (user)
+        {
+            this.loggedIn = true;
+            this.displayName = user.displayName;
+        }
+    }
 }
--- client/src/views/login.js
+++ client/src/views/login.js
@@ -1,4 +1,57 @@
+import {inject} from "aurelia-framework";
+import {Router} from "aurelia-router";
+import app from "../services";
+
+const ERR_BAD_LOGIN = "Username or password invalid";
+const ERR_INTERNAL = "Something went wrong and we had to log you out. Please log in again.";
+const INFO_SIGNUP_SUCCESS = "Your account has been created! You may now log in.";
+
+@inject(Router)
 export class Login
 {
-    
+    email = "";
+    password = "";
+
+    error = "";
+    info = "";
+
+    router;
+
+    constructor(router)
+    {
+        this.router = router;
+    }
+
+    activate(params)
+    {
+        if (params.internalError)
+        {
+            this.error = ERR_INTERNAL;
+        }
+        else if (params.signupSuccess)
+        {
+            this.info = INFO_SIGNUP_SUCCESS;
+        }
+    }
+
+    async send()
+    {
+        try
+        {
+            await app.authenticate({
+                type: "local",
+                email: this.email,
+                password: this.password
+            });
+        }
+        catch (err)
+        {
+            console.error(JSON.stringify(err));
+            this.info = "";
+            this.error = ERR_BAD_LOGIN;
+            return;
+        }
+
+        this.router.navigateToRoute("home");
+    }
 }
