e165a6c7f2c1f9477637eea81c1970fe88746a03
Stephen Cohen <stesen@outlook.com>
2016-10-13 21:09:42 -0400

    Fix variable name

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ import r from "./rethink-connection";
 
 //Make sure the database exists and start the server
 r.dbList().contains("penultima")
-    .do(tableExists => r.branch(tableExists, {created: 0}, r.dbCreate("penultima"))).run()
+    .do(dbExists => r.branch(dbExists, {created: 0}, r.dbCreate("penultima"))).run()
     .then(() =>
     {
         const app = require("./app").default;
