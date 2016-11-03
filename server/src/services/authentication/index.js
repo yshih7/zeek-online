bab8592e88324756e0c41054bd4282fe39165372
plof27 <olaf223224@gmail.com>
2016-11-03 16:54:58 -0400

    Fixed auth fixes
    its cleaner now

--- server/src/services/authentication/index.js
+++ server/src/services/authentication/index.js
@@ -6,8 +6,6 @@ export default function()
     const app = this;
 
     const config = app.get("auth");
-    config.local = {'usernameField':'loginName'};
-    console.log(JSON.stringify(config));
 
     app.configure(authentication(config));
 }
