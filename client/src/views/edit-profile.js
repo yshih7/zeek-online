5903367fabd9ab86310040f6c0f0219b6e48df5b
plof27 <olaf223224@gmail.com>
2016-11-04 01:42:59 -0400

    edit-profile written
    needs some tweaks/fixing

--- client/src/views/edit-profile.js
+++ client/src/views/edit-profile.js
@@ -1,5 +1,58 @@
-import app from "../services"
+import app, {users} from "../services"
 
 export class editProfile {
+
+    error = ""
+    info = ""
+
+    loginName = ""
+    displayName = ""
+    email = ""
+    nationality = ""
+    hideEmail
+    hideNationality
+
+    activate() {
+        const user = app.get("user");
+        this.loginName = user.loginName;
+        this.displayName = user.displayName;
+        if (user.email) {
+            this.email = user.email;
+            this.hideEmail = false;
+        } else {
+            this.hideEmail = true;
+        }
+        if (user.nationality) {
+            this.nationality = user.nationality;
+            this.hideNationality = false;
+        } else {
+            this.hideNationality = true;
+        }
+    }
+
+    async save() {
+        try {
+            var data = {"displayName":this.displayName};
+            if(!this.hideEmail) {
+                data.email = this.email;
+            } else {
+                data.email = undefined;
+            }
+            if (!this.hideNationality) {
+                data.nationality = this.nationality;
+            } else {
+                data.nationality = undefined;
+            }
+            console.log(JSON.stringify(data));
+            await users.patch(this.loginName, data);
+
+            this.error = "";
+            this.info = "Information saved!"
+        } catch (err) {
+            console.log(JSON.stringify(err));
+            this.error = "Error saving profile information.";
+            this.info = "";
+        }
+    }
 
 }
