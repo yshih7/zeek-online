fbe05ffbeaaf81187b51251fdc6878bedc239ec9
yshih7 <CSJack882@hotmail.com>
2016-11-06 16:25:59 -0500

    More map create update
    Add more UI and attempt to create a new map. Still need to add three
    more variables for map database and check for database connection error

--- client/src/views/create.js
+++ client/src/views/create.js
@@ -1,25 +1,47 @@
 import {inject} from "aurelia-framework";
 import {Router, Redirect} from "aurelia-router";
-import app from "../services";
+import app, {maps} from "../services";
 
 @inject(Router)
 export class Home
 {
-	map = [];
-	tag = "";
 	name = "";
+	map = [];
+	tags = "";
+	name = "";
+	height;
+	width;
 	
 	router;
 
 	constructor(router)
 	{
 		this.router = router;
-  }
+	}
 
 
 	async create(){
 		//create the map with the current configuration
 		// .split(/\s+/g)	
+
+		try
+        {
+            await maps.create({
+                //id:p
+				//creator
+				//createdOn:DateTime
+				name: this.name,
+				tags: this.tags.split(/\s*,\s*/g),
+				height: this.height,
+				width: this.width,
+				map: this.map.split(/\s+/g)
+            });
+        }
+		catch (err)
+        {
+            console.error(JSON.stringify(err));
+            return;
+        }
 	}
     
 }
